41afcd02a23afa1854cd3d7fd668aed6d8101168
Max Potasznik <maxp888@gmail.com>
2014-02-28 15:52:28 -0500

    working stArd

--- stArd/stArd.ino
+++ stArd/stArd.ino
@@ -2,10 +2,10 @@ bool oldb1, oldb2, b1, b2;
 unsigned long high1, high2, low1, low2, timer, lTime, hiTime;
 unsigned long PUSH_THRESH = 1000;
 unsigned long LONGPRESS_THRESH = 1000;
-//unsigned long ENTRY_THRESH = 25;
-unsigned int PRESSURE_THRESH = 250;
+unsigned long ENTRY_THRESH = 100;
+unsigned int PRESSURE_THRESH = 650;
 int btn1pin = A0;
-int btn2pin = A0;
+int btn2pin = A1;
 int row, col;
 bool delFlg, spcFlg, rowFlg, colFlg;
 char code[7][5] = {
@@ -41,7 +41,7 @@ void push(){
 		Serial.println("DEL");
 	}
 	else if (spcFlg and not delFlg) {
-		Serial.println(' ');
+		Serial.println("SPC");
 	}
   else if (delFlg and spcFlg){
     Serial.println("SEND");
@@ -50,7 +50,9 @@ void push(){
 		row = row % 7;
 		col = col % 5;
 		Serial.println(code[row][col]);
-	}
+	}else {
+    Serial.println("nothing to send");
+  }
 }
 
 void loop() {
@@ -60,39 +62,49 @@ void loop() {
   b2 = (analogRead(btn2pin) > PRESSURE_THRESH);
 
   if (b1 and not oldb1){  //POSITIVE TRANSITION
-  	high1 = lTime;
-  } 
-  else if ((not b1) and oldb1) { //NEGATIVE TRANSITION
-  	hiTime = lTime - high1;
-  	if (hiTime >= LONGPRESS_THRESH){
-  		delFlg = true;
-  		low1 = lTime;
-  	} else {
-  		rowFlg = true;
-  		row++;
-  		low1 = lTime;
-  	}
+    if (lTime - high1 > ENTRY_THRESH){
+    	high1 = lTime;
+      row++;
+      Serial.print("#R");
+      Serial.println((row % 7));
+    }
+  } else if (b1 and oldb1){
+    if ((lTime - high1) >= LONGPRESS_THRESH){
+      delFlg = true;
+      row = -1;
+      col = -1;
+    }    
+  } else if ((not b1) and oldb1) { //NEGATIVE TRANSITION
+    low1 = lTime;
   }
   
   if (b2 and not oldb2){  //POSITIVE TRANSITION
-  	high2 = lTime;
-  } 
-  else if ((not b2) and oldb2) { //NEGATIVE TRANSITION
-  	hiTime = lTime - high2;
-  	if (hiTime >= LONGPRESS_THRESH){
-  		spcFlg = true;
-  		low2 = lTime;
-  	} else {
-  		colFlg = true;
-  		col++;
-  		low2 = lTime;
-  	}
+  	if (lTime - high2 > ENTRY_THRESH){
+      high2 = lTime;
+      col++;
+      Serial.print("#C");
+      Serial.println((col % 5));
+    }
+  } else if (b2 and oldb2) {
+    if ((lTime - high2) >= LONGPRESS_THRESH){
+      spcFlg = true;
+      row = -1;
+      col = -1;
+    }
+  } else if ((not b2) and oldb2) { //NEGATIVE TRANSITION
+		low2 = lTime;
   }
 
 //both button1 and button2 
-  if (((low1 > high1) and (low2 > high2)) and ((lTime - low1 > PUSH_THRESH) and (lTime - low2 > PUSH_THRESH))){
-  	push();
-  	clear();
+  if ((not b1) and (not b2)){
+    if (spcFlg or delFlg){
+      push();
+      clear();
+    } else if ((lTime - low1 > PUSH_THRESH) and (lTime - low2 > PUSH_THRESH) 
+      and (row >= 0) and (col >= 0)){
+      push();
+      clear();
+    }
   }
 
   //store button vals
